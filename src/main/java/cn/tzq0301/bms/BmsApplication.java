c78c9fbcb84f105c9649c9997cff13079df9169d
tzq0301 <7947437+tzq0301@user.noreply.gitee.com>
2021-05-05 14:18:26 +0800

    Initial commit

---
+++ src/main/java/cn/tzq0301/bms/BmsApplication.java
@@ -0,0 +1,13 @@
+package cn.tzq0301.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/tzq0301/bms/BmsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tzq0301.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
